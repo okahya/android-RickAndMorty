0cd3f86d9716b3041278b53d6610b956ef153296
osmankahya <kahya.osman@yahoo.com>
2022-07-27 14:23:11 +0300

    [Create custom toolbar][okahya]

--- app/src/main/java/com/okahya/rickandmorty/scene/MainActivity.kt
+++ app/src/main/java/com/okahya/rickandmorty/scene/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.okahya.rickandmorty.scene
 
 import androidx.activity.viewModels
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
@@ -20,11 +21,23 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override fun initialize() {
         super.initialize()
 
+        val toolbar = binding.toolbarMain
+        setSupportActionBar(toolbar)
+
         navController = Navigation.findNavController(this, R.id.fragmentContainerView)
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.homeFragment
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
+        setNavigationBackIcon(toolbar)
+    }
+
+    private fun setNavigationBackIcon(toolbar: Toolbar) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id != R.id.homeFragment) {
+                toolbar.setNavigationIcon(R.drawable.ic_cancel)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {

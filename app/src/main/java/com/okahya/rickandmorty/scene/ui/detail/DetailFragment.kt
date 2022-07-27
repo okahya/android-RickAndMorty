85b8cd1521190383dd5cbe0c5dc4177abab99763
osmankahya <kahya.osman@yahoo.com>
2022-07-27 19:53:30 +0300

    [Add argument on detail scene][okahya]

--- app/src/main/java/com/okahya/rickandmorty/scene/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/okahya/rickandmorty/scene/ui/detail/DetailFragment.kt
@@ -3,6 +3,7 @@ package com.okahya.rickandmorty.scene.ui.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.okahya.rickandmorty.R
 import com.okahya.rickandmorty.base.ui.BaseFragment
 import com.okahya.rickandmorty.databinding.FragmentDetailBinding
@@ -14,6 +15,8 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(){
     override val layoutResourceId: Int = R.layout.fragment_detail
     override val viewModel: DetailViewModel by viewModels()
 
+    private val argument: DetailFragmentArgs by navArgs()
+
     override fun initialize() {
         super.initialize()
         binding.viewModel = viewModel
@@ -22,5 +25,19 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setCharacterInfoText()
+    }
+
+    private fun characterInfo() = argument.characterInfo
+
+    private fun setCharacterInfoText() {
+        with(binding.characterDetailView) {
+            characterInfo()?.let {
+                setCharacterName(it.name)
+                setCharacterStatusSpecies(it.status, it.species)
+                setCharacterGender(it.gender)
+                setCharacterImage(it.image)
+            }
+        }
     }
 }
